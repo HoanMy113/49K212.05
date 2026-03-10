542f630cb3ba901727465b8914577ec61076eca3
Duyên <locduyent@gmail.com>
2026-03-10 15:38:02 +0700

    Add worker model and update home controller

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace FixItNow.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Worker> Workers { get; set; }
+    }
+}
