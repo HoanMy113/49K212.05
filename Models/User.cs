314e38be467d8295022086e9498c81b20b7f50f8
DELL <DELL@BUIKHANH-PC>
2026-04-08 00:20:04 +0700

    chore: remove redundant frontend, Views, and root project files

--- Models/User.cs
+++
@@ -1,8 +0,0 @@
-public class User {
-    public int Id { get; set; }
-    public string FullName { get; set; } = "";
-    public string Phone { get; set; } = "";
-    public string PasswordHash { get; set; } = "";
-    public UserRole Role { get; set; }
-    public int? WorkerProfileId { get; set; }
-}
